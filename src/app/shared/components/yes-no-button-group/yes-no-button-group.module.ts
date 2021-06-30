c93942af02a52e01c38d6b4aece38e7278423f17
Carol Vasconcelos <cvsouza04@gmail.com>
2021-06-29 21:49:05 -0300

    (WIP) feat: diretiva para tratar gerenciar o teclado e setar o focus no elemento correto

--- src/app/shared/components/yes-no-button-group/yes-no-button-group.module.ts
+++ src/app/shared/components/yes-no-button-group/yes-no-button-group.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { YesNoButtonGroupComponent } from './yes-no-button-group.component';
 import { UniqueIdService } from '../../services/unique-id/unique-id.service';
-
-
-
+import { KeyboardManagerModule } from '../../directives/keyboard-manager/keyboard-manager.module';
 @NgModule({
   declarations: [
     YesNoButtonGroupComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    KeyboardManagerModule,
   ],
   providers: [UniqueIdService],
   exports: [YesNoButtonGroupComponent]
