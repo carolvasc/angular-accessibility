4b04c960c0228676fe52a0196141e2be6cbb52f4
Carol Vasconcelos <cvsouza04@gmail.com>
2021-07-05 20:07:51 -0300

    feat: iniciando com animações

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,6 +18,7 @@ import { ModalModule } from './shared/components/modal/modal.module';
     AppRoutingModule,
     DisableControlModule,
     ModalModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
+import { fade } from '../../animations/fade-in';
 import { ModalConfig } from './interfaces/modal-config.model';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['modal.component.less'],
+  animations: [fade],
 })
 export class ModalComponent {
+  @HostBinding('@fade') fade = true;
   public config: ModalConfig;
 }
